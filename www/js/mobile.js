5cf139a7a3989ed4eff1fbfa34822517e044ef7c
unknown <e.wamugu@CE-BACKEND-ERIC.ram.co.ke>
2015-12-02 15:34:07 +0300

    Fixed bugs

--- www/js/mobile.js
+++ www/js/mobile.js
@@ -10,8 +10,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // Cordova is ready
 function onDeviceReady() {
 	
-	db = window.openDatabase("mbeguchoicedb", "1.0", "MbeguChoice", 2 * 1024 * 1024); //100MB db
-	// db = window.sqlitePlugin.openDatabase({name: "mbeguchoicesqllitedb.db"});
+	// db = window.openDatabase("mbeguchoicedb", "1.0", "MbeguChoice", 2 * 1024 * 1024); //100MB db
+	db = window.sqlitePlugin.openDatabase({name: "mbeguchoicesqllitedb.db"});
 	//testDB();
 	populateDB();
 	populate_counties_dropdown();
