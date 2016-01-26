3cdae343f9ef3f53cde61e0e0515dd7ffc739aec
Elric <wamugu@gmail.com>
2016-01-26 11:58:07 +0300

    fixing data sync functionality

--- www/js/mobile.js
+++ www/js/mobile.js
@@ -11,9 +11,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	
 	db = window.openDatabase("mbeguchoicedb", "1.0", "MbeguChoice", 2 * 1024 * 1024); //100MB db
-	// db = window.sqlitePlugin.openDatabase({name: "mbeguchoiceappdb.db"});
-	
-	// $("#ajax-spinner").css( "display", "inline !important" );
+	db = window.sqlitePlugin.openDatabase({name: "mbeguchoiceappdb.db"});
 
 	//testDB();
 	populateDB();
@@ -21,7 +19,7 @@ function onDeviceReady() {
 	// populate_crops_dropdown(); //called on language selector after document load
 	populate_maturity_filter();
 	
-	// sync_with_live_db();
+	sync_with_live_db();
 
 	$('.generate-pdf').click(function(e) {
     	generateResultsPDF();
@@ -2340,17 +2338,7 @@ function sync_with_live_db () {
 	        db.transaction(
 				function(tx) {
 					var crop_name = crop.crop_name;
-					
-					// if(crop.crop_id == appdb_crop_ids[i]){
-					// 	var update_crops_sql = 'UPDATE default_sid_crop SET crop_name ="'+crop_name+'", category_id='+crop.category_id+' WHERE crop_id ='+crop.crop_id+';';
-					// 	tx.executeSql(update_crops_sql);
-						
-					// }else{
-					// 	var insert_crops_sql = 'INSERT OR IGNORE INTO default_sid_crop (crop_id, crop_name, category_id) VALUES '+
-					// 	'(NULL, "'+crop_name+'", '+crop.category_id+');';
-					// 	tx.executeSql(insert_crops_sql);
-					// }
-					
+										
 					var insert_crops_sql = 'INSERT OR IGNORE INTO default_sid_crop (crop_id, crop_name, category_id) VALUES '+
 					'("'+crop.crop_id+'", "'+crop_name+'", '+crop.category_id+');';
 					tx.executeSql(insert_crops_sql);
@@ -2451,35 +2439,6 @@ function sync_with_live_db () {
 		$.each( cropvarieties, function( i, cropvariety ) {
 	        db.transaction(
 				function(tx) {
-												
-					// if(cropvariety.sw_id == appdb_cropvarieties_ids[i]){
-					// 	var update_cropvarieties_sql = 'UPDATE default_sid_seedchoice_seedworks_combined SET '+
-					// 	'sw_variety = "'+cropvariety.sw_variety+'", '+
-					// 	'sw_comm_agent = "'+cropvariety.sw_comm_agent+'", '+
-					// 	'sw_alt_optimal = "'+cropvariety.sw_alt_optimal+'", '+
-					// 	'sw_maturity_age = "'+cropvariety.sw_maturity_age+'", '+
-					// 	'sw_special_attrib = "'+cropvariety.sw_special_attrib+'", '+
-					// 	'sw_drought_tolerant = "'+cropvariety.sw_drought_tolerant+'", '+
-					// 	'sw_disease_tolerant = "'+cropvariety.sw_disease_tolerant+'", '+
-					// 	'sw_storage_pest_resistant = "'+cropvariety.sw_storage_pest_resistant+'", '+
-					// 	'sw_consumer_preferences = "'+cropvariety.sw_consumer_preferences+'", '+
-					// 	'county = "'+cropvariety.county+'", '+
-					// 	'lowland = "'+cropvariety.lowland+'", '+
-					// 	'lowland_transitional = "'+cropvariety.lowland_transitional+'", '+
-					// 	'mid_altitude = "'+cropvariety.mid_altitude+'", '+
-					// 	'highland_transitional = "'+cropvariety.highland_transitional+'", '+
-					// 	'highland = "'+cropvariety.highland+'" '+
-					// 	' WHERE sw_id = '+cropvariety.sw_id+';';
-						
-					// 	// tx.executeSql(update_cropvarieties_sql);
-					// 	// console.dir(update_cropvarieties_sql);
-						
-					// }else{
-					// 	var insert_cropvarieties_sql = 'INSERT OR IGNORE INTO default_sid_seedchoice_seedworks_combined (sw_id, sw_variety, category_id, crop_id, seedtype_id, season_id, license_type_id, comm_potential_id, comm_level_id, sw_releaseyr, sw_breed_institution, sw_maintainer, sw_comm_agent, sw_alt_optimal, sw_alt_min, sw_alt_max, sw_maturity, sw_maturity_age, sw_special_attrib, sw_drought_tolerant, sw_disease_tolerant, sw_storage_pest_resistant, sw_consumer_preferences, county, lowland, lowland_transitional, mid_altitude, highland_transitional, highland) VALUES '+
-					// 	'(NULL, "'+cropvariety.sw_variety+'", "'+cropvariety.category_id+'", "'+cropvariety.crop_id+'", "'+cropvariety.seedtype_id+'", "'+cropvariety.season_id+'", "'+cropvariety.license_type_id+'", "'+cropvariety.comm_potential_id+'", "'+cropvariety.comm_level_id+'", "'+cropvariety.sw_releaseyr+'", "'+cropvariety.sw_breed_institution+'", "'+cropvariety.sw_maintainer+'", "'+cropvariety.sw_comm_agent+'", "'+cropvariety.sw_alt_optimal+'", "'+cropvariety.sw_alt_min+'", "'+cropvariety.sw_alt_max+'", "'+cropvariety.sw_maturity+'", "'+cropvariety.sw_maturity_age+'", "'+cropvariety.sw_special_attrib+'", "'+cropvariety.sw_drought_tolerant+'", "'+cropvariety.sw_disease_tolerant+'", "'+cropvariety.sw_storage_pest_resistant+'", "'+cropvariety.sw_consumer_preferences+'", "'+cropvariety.county+'", "'+cropvariety.lowland+'", "'+cropvariety.lowland_transitional+'", "'+cropvariety.mid_altitude+'", "'+cropvariety.highland_transitional+'", "'+cropvariety.highland+'"'+');';
-					// 	// tx.executeSql(insert_cropvarieties_sql);
-					// 	// console.dir(insert_cropvarieties_sql);
-					// }
 
 					var insert_cropvarieties_sql = 'INSERT OR IGNORE INTO default_sid_seedchoice_seedworks_combined (sw_id, sw_variety, category_id, crop_id, seedtype_id, season_id, license_type_id, comm_potential_id, comm_level_id, sw_releaseyr, sw_breed_institution, sw_maintainer, sw_comm_agent, sw_alt_optimal, sw_alt_min, sw_alt_max, sw_maturity, sw_maturity_age, sw_special_attrib, sw_drought_tolerant, sw_disease_tolerant, sw_storage_pest_resistant, sw_consumer_preferences, county, lowland, lowland_transitional, mid_altitude, highland_transitional, highland) VALUES '+
 					'("'+cropvariety.sw_id+'", "'+cropvariety.sw_variety+'", "'+cropvariety.category_id+'", "'+cropvariety.crop_id+'", "'+cropvariety.seedtype_id+'", "'+cropvariety.season_id+'", "'+cropvariety.license_type_id+'", "'+cropvariety.comm_potential_id+'", "'+cropvariety.comm_level_id+'", "'+cropvariety.sw_releaseyr+'", "'+cropvariety.sw_breed_institution+'", "'+cropvariety.sw_maintainer+'", "'+cropvariety.sw_comm_agent+'", "'+cropvariety.sw_alt_optimal+'", "'+cropvariety.sw_alt_min+'", "'+cropvariety.sw_alt_max+'", "'+cropvariety.sw_maturity+'", "'+cropvariety.sw_maturity_age+'", "'+cropvariety.sw_special_attrib+'", "'+cropvariety.sw_drought_tolerant+'", "'+cropvariety.sw_disease_tolerant+'", "'+cropvariety.sw_storage_pest_resistant+'", "'+cropvariety.sw_consumer_preferences+'", "'+cropvariety.county+'", "'+cropvariety.lowland+'", "'+cropvariety.lowland_transitional+'", "'+cropvariety.mid_altitude+'", "'+cropvariety.highland_transitional+'", "'+cropvariety.highland+'"'+');';
@@ -2490,15 +2449,176 @@ function sync_with_live_db () {
 			);
      	});
 		
-		$('#language-menu-link').trigger('click');
+		// $('#language-menu-link').trigger('click');
 		
 	}).fail(function(jqxhr, textStatus, error) {
 	    var err = textStatus + ", " + error;
 		console.dir( "Request Failed: " + err );
 	});
 
+	$.ajax({
+		async: true,
+	    type: "GET",
+	    url: "http://mbeguchoice.com/swahili/mbegu_choice/get_mobile_app_sync_data",
+	    dataType: "json"
+	}).done(function( sync_data, textStatus, jqXHR ) {
+		
+		var crops 			= $.parseJSON(sync_data.crops);
+        var cropcategories 	= $.parseJSON(sync_data.cropcategories);
+        var institutions 	= $.parseJSON(sync_data.institutions);
+        var cropvarieties 	= $.parseJSON(sync_data.cropvarieties);
+
+        var appdb_crop_ids = [];
+        db.transaction(
+			function(tx) {		
+				tx.executeSql('SELECT crop_id FROM default_sid_crop_swa order by crop_id asc', [], function (tx, results) {
+				  	var length = results.rows.length, i;
+				  	for (i = 0; i < length; i++) {
+				    	appdb_crop_ids.push(results.rows.item(i).crop_id);
+				  	}
+				});
+			},
+			function(error){console.dir(error);}
+		);
+		
+        //update/insert into crops table
+        db.transaction(
+			function(tx) {
+				var truncate_cropstable_sql = "DROP TABLE IF EXISTS default_sid_crop_swa;";
+				tx.executeSql(truncate_cropstable_sql);
+
+				var create_cropstable_sql = "CREATE TABLE IF NOT EXISTS default_sid_crop_swa ( "+
+					"crop_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+					"crop_name varchar(255), " + 
+					"category_id INTEGER(11) " + 
+				");";
+        		tx.executeSql(create_cropstable_sql);
+			},
+			function(error){console.dir(error);}
+		);
+
+		$.each( crops, function( i, crop ) {
+	        db.transaction(
+				function(tx) {
+					var crop_name = crop.crop_name;
+										
+					var insert_crops_sql = 'INSERT OR IGNORE INTO default_sid_crop_swa (crop_id, crop_name, category_id) VALUES '+
+					'("'+crop.crop_id+'", "'+crop_name+'", '+crop.category_id+');';
+					tx.executeSql(insert_crops_sql);
+				},
+				function(error){console.dir(error);}
+			);
+
+     	 });
+
+		var appdb_cropcategories_ids = [];
+        db.transaction(
+			function(tx) {		
+				tx.executeSql('SELECT category_id FROM default_sid_cropcategory_swa order by category_id asc', [], function (tx, results) {
+				  	var length = results.rows.length, i;
+				  	for (i = 0; i < length; i++) {
+				    	appdb_cropcategories_ids.push(results.rows.item(i).category_id);
+				  	}
+				});
+			},
+			function(error){console.dir(error);}
+		);
+
+		//update/insert into cropcategories table
+		$.each( cropcategories, function( i, cropcategory ) {
+	        db.transaction(
+				function(tx) {
+					var category_name = cropcategory.category_name;
+					
+					if(cropcategory.category_id == appdb_cropcategories_ids[i]){
+						var update_cropcategories_sql = 'UPDATE default_sid_cropcategory_swa SET category_name ="'+category_name+'"'+' WHERE category_id ='+cropcategory.category_id+';';
+						tx.executeSql(update_cropcategories_sql);
+						
+					}else{
+						var insert_cropcategories_sql = 'INSERT OR IGNORE INTO default_sid_cropcategory_swa (category_id, category_name) VALUES '+
+						'(NULL, "'+category_name+'"'+');';
+						tx.executeSql(insert_cropcategories_sql);
+					}
+				},
+				function(error){console.dir(error);}
+			);
+     	 });
+
+		var appdb_cropvarieties_ids = [];
+        db.transaction(
+			function(tx) {		
+				tx.executeSql('SELECT sw_id FROM default_sid_seedchoice_seedworks_combined_swa order by sw_id asc', [], function (tx, results) {
+				  	var length = results.rows.length, i;
+				  	for (i = 0; i < length; i++) {
+				    	appdb_cropvarieties_ids.push(results.rows.item(i).sw_id);
+				  	}
+				});
+			},
+			function(error){console.dir(error);}
+		);
+
+		//update/insert into cropvarieties table
+		db.transaction(
+			function(tx) {
+				var truncate_cropsvarietiestable_sql = "DROP TABLE IF EXISTS default_sid_seedchoice_seedworks_combined_swa;";
+				tx.executeSql(truncate_cropsvarietiestable_sql);
+
+				var create_cropsvarietiestable_sql = "CREATE TABLE IF NOT EXISTS default_sid_seedchoice_seedworks_combined_swa ( "+
+					"sw_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+					"sw_variety varchar(255),"+
+					"category_id INTEGER(11),"+
+					"crop_id INTEGER(11),"+
+					"seedtype_id INTEGER(11),"+
+					"season_id INTEGER(11),"+
+					"license_type_id INTEGER(11),"+
+					"comm_potential_id INTEGER(11),"+
+					"comm_level_id INTEGER(11),"+
+					"sw_releaseyr INTEGER(11),"+
+					"sw_breed_institution varchar(255),"+
+					"sw_maintainer varchar(255),"+
+					"sw_comm_agent varchar(255),"+
+					"sw_alt_optimal varchar(255),"+
+					"sw_alt_min INTEGER(11),"+
+					"sw_alt_max INTEGER(11),"+
+					"sw_maturity varchar(255),"+
+					"sw_maturity_age varchar(50),"+
+					"sw_special_attrib varchar(255),"+
+					"sw_drought_tolerant varchar(10),"+
+					"sw_disease_tolerant varchar(10),"+
+					"sw_storage_pest_resistant varchar(10),"+
+					"sw_consumer_preferences varchar(10),"+
+					"county TEXT,"+
+					"lowland TEXT,"+
+					"lowland_transitional TEXT,"+
+					"mid_altitude TEXT,"+
+					"highland_transitional TEXT,"+
+					"highland TEXT "+ 
+				");";
+        		tx.executeSql(create_cropsvarietiestable_sql);
+			},
+			function(error){console.dir(error);}
+		);
+
+		$.each( cropvarieties, function( i, cropvariety ) {
+	        db.transaction(
+				function(tx) {
+
+					var insert_cropvarieties_sql = 'INSERT OR IGNORE INTO default_sid_seedchoice_seedworks_combined_swa (sw_id, sw_variety, category_id, crop_id, seedtype_id, season_id, license_type_id, comm_potential_id, comm_level_id, sw_releaseyr, sw_breed_institution, sw_maintainer, sw_comm_agent, sw_alt_optimal, sw_alt_min, sw_alt_max, sw_maturity, sw_maturity_age, sw_special_attrib, sw_drought_tolerant, sw_disease_tolerant, sw_storage_pest_resistant, sw_consumer_preferences, county, lowland, lowland_transitional, mid_altitude, highland_transitional, highland) VALUES '+
+					'("'+cropvariety.sw_id+'", "'+cropvariety.sw_variety+'", "'+cropvariety.category_id+'", "'+cropvariety.crop_id+'", "'+cropvariety.seedtype_id+'", "'+cropvariety.season_id+'", "'+cropvariety.license_type_id+'", "'+cropvariety.comm_potential_id+'", "'+cropvariety.comm_level_id+'", "'+cropvariety.sw_releaseyr+'", "'+cropvariety.sw_breed_institution+'", "'+cropvariety.sw_maintainer+'", "'+cropvariety.sw_comm_agent+'", "'+cropvariety.sw_alt_optimal+'", "'+cropvariety.sw_alt_min+'", "'+cropvariety.sw_alt_max+'", "'+cropvariety.sw_maturity+'", "'+cropvariety.sw_maturity_age+'", "'+cropvariety.sw_special_attrib+'", "'+cropvariety.sw_drought_tolerant+'", "'+cropvariety.sw_disease_tolerant+'", "'+cropvariety.sw_storage_pest_resistant+'", "'+cropvariety.sw_consumer_preferences+'", "'+cropvariety.county+'", "'+cropvariety.lowland+'", "'+cropvariety.lowland_transitional+'", "'+cropvariety.mid_altitude+'", "'+cropvariety.highland_transitional+'", "'+cropvariety.highland+'"'+');';
+					tx.executeSql(insert_cropvarieties_sql);
+				},
+				function(error){console.dir(error);}
+			);
+     	});
+		
+	}).fail(function(jqxhr, textStatus, error) {
+	    var err = textStatus + ", " + error;
+		console.dir( "Request Failed for Swahili: " + err );
+	});
+
 	$(document).ajaxComplete(function(event, request, settings){
   		$("#sync a").removeClass("blink");
+  		$('#language-menu-link').trigger('click');
     });
 	
 }
